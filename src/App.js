f7c92394e7b3ce3f9255a9a0a91c602c283d5163
Si11ySoliloquy <114286745+Si11ySoliloquy@users.noreply.github.com>
2023-04-28 05:09:28 -0700

    1 file modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, {useEffect, useState} from "react";
-import "./style.css";
-import coke from './images/coke.jpg';
+import React, { useEffect, useState } from 'react';
+import './style.css';
+import coke from './z_images/coke.jpg';
 
 export default function App() {
   return (
     <div>
       <style>{'body { background-color:#23272e; }'}</style>
-      <h1 style ={{color:'white'}}>My Portfolio!</h1>
-      <p style ={{color:'white'}}>Start editing to see some magic happen :)</p>
-      <h2 style ={{color:'white'}}> THIS IS A LINK</h2>
-      <a href="https://twitter.com/supersillly/status/1651907768654213129" style ={{color:'white'}}>  
-      <img src={coke} alt="Twitter Logo"/>
-
+      <h1 style={{ color: 'white' }}>My Portfolio!</h1>
+      <p style={{ color: 'white' }}>
+        Start editing to see some magic happen :)
+      </p>
+      <h2 style={{ color: 'white' }}> THIS IS A LINK</h2>
+      <a
+        href="https://twitter.com/supersillly/status/1651907768654213129"
+        style={{ color: 'white' }}
+      >
+        <img src={coke} alt="Twitter Logo" />
       </a>
     </div>
-    
   );
 }
