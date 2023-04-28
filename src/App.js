d64e287694330ae57cb5246aa74cf5dbdfaaa284
Si11ySoliloquy <114286745+Si11ySoliloquy@users.noreply.github.com>
2023-04-28 04:03:57 -0700

    changed background, learning html tags and css stuff

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <style>{'body { background-color:#23272e; }'}</style>
+      <h1 style ={{color:'white'}}>My Portfolio!</h1>
+      <p style ={{color:'white'}}>Start editing to see some magic happen :)</p>
     </div>
   );
 }
