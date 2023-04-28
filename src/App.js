03288916a78375245e852410831a5d760315f705
Si11ySoliloquy <114286745+Si11ySoliloquy@users.noreply.github.com>
2023-04-28 04:49:21 -0700

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./style.css";
 
 export default function App() {
@@ -7,6 +7,12 @@ export default function App() {
       <style>{'body { background-color:#23272e; }'}</style>
       <h1 style ={{color:'white'}}>My Portfolio!</h1>
       <p style ={{color:'white'}}>Start editing to see some magic happen :)</p>
+      <h2 style ={{color:'white'}}> THIS IS A LINK</h2>
+      <a href="https://twitter.com/supersillly/status/1651907768654213129" style ={{color:'white'}}>  
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/twitter/twitter-original.svg" alt="Twitter Logo">
+        </img>
+      </a>
     </div>
+    
   );
 }
